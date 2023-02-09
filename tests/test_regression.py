ffa9f85a5b5eedf7238857feb03b50439753b92d
Helmut Konrad Schewe <helmutus@outlook.com>
2023-02-09 15:22:58 +0000

    test(regression): verify outlines

---
+++ tests/test_regression.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2023 by Helmut Konrad Schewe. All rights reserved.
+# This file is property of Helmut Konrad Schewe. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import hardcore
+import pytest
+
+import tests
+
+NO_PDF = __file__
+
+
+@pytest.mark.parametrize('cmd', [
+    pytest.param(f'-i {hardcore.single("outline_arabic_bytes")}', id='outline'),
+])
+def test_regression(cmd, td, mp):  #pylint: disable=W0613
+    tests.run(cmd, mp=mp)
